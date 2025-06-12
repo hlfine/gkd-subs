017e9b0c0f635291b15502b9cc67b03528f6b4c3
hanl <hanlei0706@rayootech.com>
2025-06-12 15:12:08 +0800

    release: 🎉

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,10 +4,10 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 6783,
+  name: 'hlfine-gkd-subscription',
   version: 0,
-  author: 'author',
+  author: 'hlfine',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
